9d62fe25c8c8adc1fd58a0c30740d5ec4d7b25e3
jairo bezerra de lima <jairo@MacBook-Air-de-jairo.local>
2020-03-21 17:20:52 -0300

    ajustes para deploy

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -11,14 +11,14 @@ class SessionController {
       email: Yup.string()
         .email()
         .required(),
-      password: Yup.string().required(),
+      password_hash: Yup.string().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
-    const { email, password } = req.body;
+    const { email, password_hash } = req.body;
 
     const user = await User.findOne({
       where: { email },
@@ -34,9 +34,6 @@ class SessionController {
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
     }
-    if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Password does not match' });
-    }
 
     const { id, name, phone, avatar, provider } = user;
 
@@ -48,6 +45,7 @@ class SessionController {
         email,
         provider,
         avatar,
+        password_hash,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -9,7 +9,7 @@ class UserController {
       email: Yup.string()
         .email()
         .required(),
-      password: Yup.string()
+      password_hash: Yup.string()
         .required()
         .min(6),
       phone: Yup.string()
@@ -21,13 +21,22 @@ class UserController {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
-    const userExists = await User.findOne({ where: { email: req.body.email } });
+    const userExists = await User.findOne({
+      where: { email: req.body.email },
+    });
 
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
     }
 
-    const { id, name, email, provider, phone } = await User.create(req.body);
+    const {
+      id,
+      name,
+      email,
+      provider,
+      phone,
+      password_hash,
+    } = await User.create(req.body);
 
     return res.json({
       id,
@@ -35,6 +44,7 @@ class UserController {
       email,
       provider,
       phone,
+      password_hash,
     });
   }
 
@@ -43,22 +53,14 @@ class UserController {
       name: Yup.string(),
       email: Yup.string().email(),
       phone: Yup.string().min(8),
-      oldPassword: Yup.string().min(6),
-      password: Yup.string()
-        .min(6)
-        .when('oldPassword', (oldPassword, field) =>
-          oldPassword ? field.required() : field
-        ),
-      confirmPassword: Yup.string().when('password', (password, field) =>
-        password ? field.required().oneOf([Yup.ref('password')]) : field
-      ),
+      password_hash: Yup.string().min(6),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
-    const { email, oldPassword } = req.body;
+    const { email } = req.body;
 
     const user = await User.findByPk(req.userId);
 
@@ -70,25 +72,34 @@ class UserController {
       }
     }
 
-    if (oldPassword && !(await user.checkPassword(oldPassword))) {
-      return res.status(401).json({ error: 'Password does not match' });
-    }
     await user.update(req.body);
 
-    const { id, name, avatar, phone, provider } = await User.findByPk(
-      req.userId,
-      {
-        include: [
-          {
-            model: File,
-            as: 'avatar',
-            attributes: ['id', 'path', 'url'],
-          },
-        ],
-      }
-    );
+    const {
+      id,
+      name,
+      avatar,
+      phone,
+      provider,
+      password_hash,
+    } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
-    return res.json({ id, name, email, avatar, provider, phone });
+    return res.json({
+      id,
+      name,
+      email,
+      avatar,
+      provider,
+      phone,
+      password_hash,
+    });
   }
 }
 
