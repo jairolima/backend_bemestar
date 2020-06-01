d19cfa01fa6842c9dd8f38874db84dd590064c4a
jairo bezerra de lima <jairo@MacBook-Air-de-jairo.local>
2020-06-01 07:43:18 -0300

    pediatra

--- src/app/controllers/ProviderController.js
+++ src/app/controllers/ProviderController.js
@@ -7,7 +7,7 @@ class ProviderController {
       // where provider: true / trocado para id dos providers que eu quero
       // producao where: { id: ['11', '4', '5', '13', '10', '9', '3', '34'] },
       // dev where: { id: ['5', '3'] },
-      where: { id: ['11', '4', '5', '13', '10', '9', '3', '34'] },
+      where: { id: ['11', '4', '5', '13', '10', '9', '3', '34', '98'] },
       attributes: ['id', 'name', 'phone', 'email', 'avatar_id'],
       include: [
         {
