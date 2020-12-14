7b605655ae71979b633dd6b59f033f93977c0538
Jairo Lima <msgjairo@gmail.com>
2020-12-14 16:05:20 -0300

    test5 msg

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -385,7 +385,7 @@ class AppointmentController {
     // );
 
     axios.get(
-      `https://api.dr.help/message?number=5583988736747&message=Este é um lembrete, você acabou de agendar ${filter} para às ${hourStart.getHours()}:${hourStart.getMinutes()} no dia ${hourStart.getDate()}/${hourStart.getMonth()}h&token=${process.env.ZAP_TOKEN}`
+      `https://api.dr.help/message?number=5583988736747&message=Este é um lembrete, você acabou de agendar ${filter} para às ${hourStart.getHours()}:${hourStart.getMinutes()}h no dia ${hourStart.getDate()}/${hourStart.getMonth() + 1}&token=${process.env.ZAP_TOKEN}`
     );
 
     // axios.get(
