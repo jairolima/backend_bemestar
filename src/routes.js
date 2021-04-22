06d609abc5f5d22aa4a9ee82105cee6b645aebd6
Jairo Lima <msgjairo@gmail.com>
2021-04-22 00:02:10 -0300

    teste

--- src/routes.js
+++ src/routes.js
@@ -69,6 +69,44 @@ var resume = cron.schedule('0 8 * * *', () => {
 
 resume.start();
 
+var doFor = cron.schedule('0 9 * * *', () => {
+
+  console.log('Running a job at 09:00 at America/Sao_Paulo timezone');
+
+  const patients = {
+    "rows": [
+      {
+        "ID": 3231,
+        "Cliente": "Nome do cliente 1",
+        "Prestador": "Dra. Verônica Terrazas",
+        "Data": "dia 23 de abril, às  08:00h",
+        "CPF": "105.198.724-84",
+        "Telefone": "5583988736747",
+      },
+      {
+        "ID": 3232,
+        "Cliente": "Nome do cliente 2",
+        "Prestador": "Dra. Verônica Terrazas",
+        "Data": "dia 23 de abril, às  08:15h",
+        "CPF": "105.198.724-84",
+        "Telefone": "558391389448",
+      },
+    ]
+  }
+
+  patients.map((patient) => (
+    axios.get(
+      `https://api.dr.help/message?number=${patient.rows.Telefone}&message=Teste disparo confirmacao clientes&token=${process.env.ZAP_TOKEN}`
+    )
+  ))
+
+}, {
+  scheduled: true,
+  timezone: "America/Sao_Paulo"
+});
+
+doFor.start();
+
 
 var doTask = cron.schedule('0 18 * * *', () => {
   console.log("18h")
